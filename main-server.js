a257ba58c603c780be1e9b805828e119dbc89651
danielezraa <80195225+danielezraa@users.noreply.github.com>
2022-04-03 21:25:28 +0300

    update

--- main-server.js
+++ main-server.js
@@ -7,8 +7,12 @@ const axios = require("axios");
 const googleMapsKey = "AIzaSyB9mAs9XA7wtN9RdKMKRig7wlHBfUtjt1g";
 // const distance = require("google-distance-matrix");
 const munkres = require("munkres-js");
-const IP_ADDRESS = "http://10.0.0.40:3001"; // Daniel -> 10.100.102.233 // ZIV-> 10.0.0.40 // https://betteride-main-server-3mmcqmln7a-ew.a.run.app/
+const IP_ADDRESS = "http://10.100.102.233:3001"; // Daniel -> 10.100.102.233 // ZIV-> 10.0.0.40 // https://betteride-main-server-3mmcqmln7a-ew.a.run.app/
 var distance = require('./distanceMatrix/index.js');
+
+// automation vars
+let isAutomated = false;
+const automatedActiveUsersIDs = ["106239502123201988788", "106431065342803359216", "106431065342803359221", "106431065342803359233", "112665819530754433510"]
 
 app.use(cors({ origin: true }));
 app.listen(3002, async () => {
@@ -31,9 +35,17 @@ app.get("/api/OrderVehicle", async (req, res) => {
 
   // find the nearest vehicle and assign it to the user
   const assignment = await assignVehicleToUser(userOrigin, userDestination, userID);
-  console.log(assignment);
+  console.log("orderVehicle new assignmnet = ",assignment);
 
   res.send(JSON.stringify(assignment)).status(200);
+});
+
+
+app.put('/api/updateFinishedUsersAutomation', async (req, res) => {
+  const { userID } = req.query;
+  console.log("userID" + userID + " has finished trip")
+  automatedActiveUsersIDs.push(userID);
+  res.send("OK").status(200)
 });
 
 app.put('/api/generateRouteToVehicle', async (req, res) => {
@@ -44,6 +56,7 @@ app.put('/api/generateRouteToVehicle', async (req, res) => {
     const userDirections = await fetch(`${IP_ADDRESS}/getUserDirections?userID=${userID}`)
     origin_destination = await userDirections.json();
     // getting the route from the main server
+    console.log("pushing route to vehicle", origin_destination.userOrigin, origin_destination.userDestination)
     const route = await getDirectionsByAddress(origin_destination.userOrigin, origin_destination.userDestination)
     route['user_id'] = userID;
     // push to the vehicle via firebase server
@@ -75,10 +88,16 @@ app.put('/api/generateRouteToVehicle', async (req, res) => {
   }
 
   catch (e) {
-    sendLog("Somthing went wrong trying pushing route to vehicle: " + origin_destination.state.assigned + " to user-id: " + userID, "ERROR")
+    sendLog("Somthing went wrong trying pushing route to vehicle, to user-id: " + userID, "ERROR")
     console.log(e)
     res.status(400).send("ERROR")
   }
+});
+
+app.put('/api/setAutomation', async (req, res) => {
+  isAutomated = !isAutomated;
+  automationAlgorithm();
+  res.status(200).send("isAutomated set to " + isAutomated)
 });
 
 
@@ -95,10 +114,9 @@ const initiateMatrix = (vehiclesLength, usersLength) => {
   );
 };
 const getDirectionsByAddress = async (from, to) => {
+  const uri = encodeURI(`https://maps.googleapis.com/maps/api/directions/json?origin=${from}&destination=${to}&key=${googleMapsKey}`)
   return await axios
-    .get(
-      `https://maps.googleapis.com/maps/api/directions/json?origin=${from}&destination=${to}&key=${googleMapsKey}`
-    )
+    .get(uri)
     .then((response) => {
       return response.data?.routes[0]?.legs[0];
     })
@@ -230,14 +248,40 @@ const assignVehicleToUser = async (userOrigin, userDestination, userID) => {
   response = await fetch(`${IP_ADDRESS}/getVehiclesTowardsUsers`);
   const vehicles = await response.json();
 
+  console.log("trying to assign vehicle to user")
+  console.log("users",users)
+  console.log("vehicles",vehicles)
+  
   // get distance matrix by users(destinations) and vehicles(origins)
   const distanceMatrix = await createDistanceMatrix(users, vehicles);
+  if (!checkDistanceMatrix(distanceMatrix)) return 0;
 
   // send destance matrix to hungarian algorithm
   const optimized = await optimizedAssignedVehicles(distanceMatrix, vehicles, users);
+  // find user assigned vehicle id
+  let vehiclePlateNumber;
+  for (let i = 0; i < optimized.length; i++) {
+    if (optimized[i][1] == users.length - 1) {
+      vehiclePlateNumber = vehicles[optimized[i][0]].id
+      break;
+    }
+  }
 
   // reassign all vehicles available according to the optimized array of matches
   reassignVehicles(optimized, vehicles, users);
+
+  // update user state, adding origin, destantion
+  await fetch(`${IP_ADDRESS}/pushTripLocationsToUser`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userID, userOrigin, userDestination, vehiclePlateNumber })
+  });
+
+
+
+
 
   // return to the user the vehicle 
   return 1;
@@ -318,13 +362,22 @@ const assignVehicleToUser = async (userOrigin, userDestination, userID) => {
   // return sortedNearestVehicles[0][1].vehicle.plateNumber;
 };
 
-
+const checkDistanceMatrix = (matrix) => {
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] == "NaN") {
+      console.log("ERROR, notice some vehicles are not reachable")
+      sendLog(`createDistanceMatrix: notice some vehicles are not reachable`, 'ERROR');
+      return false;
+    }
+  }
+  return true;
+}
 
 const createDistanceMatrix = async (users, vehicles) => {
   const origins = vehicles.map(vehicle => vehicle.currentLocation)
   const destinations = users.map(user => user.currentLocation);
   const distanceMatrix = initiateMatrix(destinations.length, origins.length);
-
+  console.log('origin : ' + origins, "destination: " + destinations)
   distance.key('AIzaSyAEDK9co1lmhgQ2yyb6C0iko4HE7sXaK38');
   const google_matrix = await distance.matrix(origins, destinations, (err, distances) => { console.log('') });
   // console.log('distance_matrix:', google_matrix.rows);
@@ -433,3 +486,39 @@ const sendLog = async (text, type) => {
 //     // return distanceMatrix;
 //   });
 // };
+
+
+const automationAlgorithm = async () => {
+  // const addresses = ['Holon, Israel', '13 Masrik Blvd., Tel Aviv, Israel', '3 Am Veolamo, Jerusalem, Israel', '10 Pinsker, Hadera, Israel', '28 Bialik St., Ramat Gan, Israel', '11 Hayetzira, Or Yehuda ,Israel', '7 Shlomo Rd., Tel Aviv, Israel', '8 Hadassim, Hod Hasharon, Israel', '8 Haalia Harishona, Hadera, Israel', '10 Hazon Zion, Jerusalem, Israel', '23 Moshe Even Ezra, Ashdod, Israel', '50A Herzl St., Bnei Brak, Israel', '11 Gazit, Petah Tikva, Israel', '30 Frenkel Yedidia, Tel Aviv, Israel', '10 Hapisga, Jerusalem, Israel', '53 Hashomrim, Rehovot, Israel', '14 Hamaagal St., Hod Hasharon, Israel', '7 Plotitsky St., Rishon Lezion, Israel', '49 Golomb Eliahu, Tel Aviv, Israel', '29 Hanapach, Haifa, Israel', '21 1057 St., Nazareth, Israel', '34 M. Goshen Blvd., Kiryat Motzkin, Israel', '3 Mordei Hagetaot St., Hadera, Israel']
+  const addresses = ['Holon, Israel', 'Haifa, Israel' , 'Tel Aviv, Israel', 'Jerusalem, Israel','Eilat, Israel','Sderot, Israel','Neve Yam, Israel', 'Atlit, Israel', 'Nahariya Israel', 'Herzliya, Israel', 'Ein Hod, Israel' ,'Kfar Yona, Israel', 'Zikhron Yaakov', 'Hadera, Israel', 'Tirat Karmel, Israel', 'Akko, Israel', 'Rosh Hanikra, Israel', 'Kfar Blum, Israel', 'Kfar Saba, Israel', 'Mizpe Ramon, Israel', 'Rishon Lezion, Israel']
+  // const usersIDs = ["106239502123201988788","106431065342803359216","106431065342803359221","106431065342803359233","112665819530754433510"]
+  while (isAutomated) {
+    let origin, destination, userIDindex = null;
+    if (automatedActiveUsersIDs.length > 1) {
+      // generate origin and destination
+      while (origin == null || destination == null || origin == destination) {
+        origin = addresses[Math.floor(Math.random() * (addresses.length - 1))]
+        destination = addresses[Math.floor(Math.random() * (addresses.length - 1))]
+      }
+
+      // choose userID
+      userIDindex = (Math.floor(Math.random() * (automatedActiveUsersIDs.length - 1)))
+      console.log("creating automation")
+      console.log("origin",origin)
+      console.log("destination ", destination)
+      console.log("userIndex",userIDindex)
+      console.log("userID",automatedActiveUsersIDs[userIDindex])
+      // order vehicle
+      await assignVehicleToUser(origin, destination, automatedActiveUsersIDs[userIDindex]);
+
+      // remove userID from list
+      automatedActiveUsersIDs.splice(userIDindex, 1); // 2nd parameter means remove one item only
+
+    }
+    else{
+      console.log("no available vehicles, waiting for 2 seconds")
+      await delay(2000);
+    }
+  }
+}
+const delay = ms => new Promise(res => setTimeout(res, ms))
